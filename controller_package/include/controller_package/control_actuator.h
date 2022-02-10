e11c581de03b625c72213c9d092d60662acc4c85
ErikRowe <ekrowe@stud.ntnu.no>
2022-02-10 16:32:12 +0100

    created control_node with a subscriber. preliminary code names are not final

---
+++ controller_package/include/controller_package/control_actuator.h
@@ -0,0 +1,17 @@
+
+class Actuation
+{
+private:
+    /* data */
+public:
+    Actuation(/* args */);
+    ~Actuation();
+};
+
+Actuation::Actuation(/* args */)
+{
+}
+
+Actuation::~Actuation()
+{
+}
