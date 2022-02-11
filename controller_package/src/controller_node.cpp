76fc313a1699b4a130a1417d89dabf87bedc6c76
ErikRowe <ekrowe@stud.ntnu.no>
2022-02-11 12:31:33 +0100

    minor fixes to control_node

--- controller_package/src/controller_node.cpp
+++ controller_package/src/controller_node.cpp
@@ -3,12 +3,13 @@
 #include <memory>
 #include <cstdio>
 
-// Ros includes
+// Ros includes, these need to be included in dependencies
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <sensor_msgs/msg/joy.hpp>
 
 // Control group includes
-#include "controller_package/control_actuator.h"
+#include <controller_package/control_actuator.h>
 
 
 using std::placeholders::_1;
@@ -16,7 +17,12 @@ using std::placeholders::_1;
 class ControlNode : public rclcpp::Node
 {
 private:
-  void joystick_callback(const std_msgs::msg::String::SharedPtr msg) const
+  // Private variables
+  
+
+  // Private functions
+  float linear_, angular;
+  void joystick_callback(const sensor_msgs::msg::Joy::SharedPtr msg) const
   {
     /**
      * @brief Callback for the joystick reading
@@ -24,9 +30,10 @@ private:
      * TODO: make code to determine what input from the controller is sendt to controller input
      * 
      */
-    printf("got callback. This will only be printed at sysexit")
+    
   }
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  // Private ROS2 declerations
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
   
 public:
   ControlNode()
@@ -38,9 +45,9 @@ public:
      * TODO: Make a subscriber in the same node to handle actuation data that is to be sendt out from the node.
      * 
      */
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
+    joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
       "joystick_topic", 10, std::bind(&ControlNode::joystick_callback, this, _1)
-    §);
+    );
   }
 };
 
