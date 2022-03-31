50c56c3a7b05254b02dcc2fb66e52c55462b90b6
ErikRowe <ekrowe@stud.ntnu.no>
2022-03-31 15:11:23 +0200

    nothing new

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,33 +1,57 @@
 import numpy as np
+from scipy.integrate import odeint
 
 # Eq: Mṽ + C(v)v +D(v)v + g(mu) + g_0 = Tau + Tau_wind + Tau_wave # might be wrong
 
-tau = 0
-tau_wind = 0
-tau_wave = 0
 
 def  inertiaMatrix(): # System inertia matrix
-    M = np.array([[0],
+    _m = np.array([[0],
                   [0],
                   [0],
                   [0],
                   [0],
                   [0]])
-    return M
+    return _m
 
 def coriolisMatrix(): # Matrix of coriolis en sentrifugal terms
-    C = np.array([])
-    return C
+    _c = np.array([])
+    return _c
 
 def dampingMatrix(): # Matrix of dissipated damping terms
-    D = np.array([])
-    return D
+    _d = np.array([])
+    return _d
+
+def disturbances():
+    _tau = np.array([[],
+                    [],
+                    [],
+                    [],
+                    [],
+                    []])
+    return _tau
 
 def movementVector(x_n, y_n, z_n, phi_N, theta_n, psi_n):
-    v = np.array([x_n, y_n, z_n, phi_N, theta_n, psi_n]) # Might need another []
-    return v
+    _v = np.array([[x_n],
+                  [y_n],
+                  [z_n],
+                  [phi_N],
+                  [theta_n],
+                  [psi_n]])
+    return _v
 
+def blueROV2Heavy():
+    M_rigid_body = 0
+    v_dot = 0
+    C_rigid_body = 0
+    v = 0
+
+    M_a = 0
+    v_world = 0
+    C_a = 0
+    D_world = 0
+    g_nu = 0
+    tau = 0
+    dydt = M_rigid_body * v_dot + C_rigid_body * v + M_a * v_world + C_a * v_world + D_world * v_world * g_nu - tau
 
 if __name__ == "__main__":
-    v = movementVector(1,1,1,1,1,1)
-    print(v)
+    blueROV2Heavy()
